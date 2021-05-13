d31fa34daf5c912af6b8517a5c34ebd7d45c9a21
rboszko <rafa@hotmail.sk>
2021-05-13 14:14:03 +0200

    005 - add repository for item entity (add new functionality, improve others)

--- src/Repository/ItemRepository.php
+++ src/Repository/ItemRepository.php
@@ -8,12 +8,20 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 
 class ItemRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Item::class);
     }
 
-    public function findByParams(array $params)
+    /**
+     * @param array $params
+     *
+     * @return array
+     */
+    public function findByParams(array $params): array
     {
         $hasMoreThan = $params['has_more_than'] ?? null;
         $hasStock = $params['has_stock'] ?? null;
@@ -21,19 +29,34 @@ class ItemRepository extends ServiceEntityRepository
         $queryBuilder = $this->createQueryBuilder('i');
 
         if ($hasMoreThan !== null) {
-            $queryBuilder->andWhere('i.amount > :value')
-                         ->setParameter('value', $hasMoreThan);
+            $queryBuilder->andWhere('i.amount > :value')->setParameter('value', $hasMoreThan);
         } elseif ($hasStock !== null) {
             if ($hasStock === 'true') {
-                $queryBuilder->andWhere('i.amount > :value')
-                             ->setParameter('value', 0);
+                $queryBuilder->andWhere('i.amount > :value')->setParameter('value', 0);
             } else {
-                $queryBuilder->andWhere('i.amount = :value')
-                             ->setParameter('value', 0);
+                $queryBuilder->andWhere('i.amount = :value')->setParameter('value', 0);
             }
         }
 
-        return $queryBuilder->getQuery()
-                            ->getResult();
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Item|null
+     */
+    public function findByName(string $name, int $id = null): ?Item
+    {
+        $queryBuilder = $this->createQueryBuilder('i')
+                             ->andWhere('i.name = :name')
+                             ->setParameter('name', $name);
+
+        if ($id !== null) {
+            $queryBuilder->andWhere('i.id != :id')
+                         ->setParameter('id', $id);
+        }
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
     }
 }
